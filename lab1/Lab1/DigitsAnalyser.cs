98d4010a3fefc9e7e533e3316935c7f08512abf2
Valeria2998 <velx8@mail.ru>
2018-03-27 10:20:01 +0400

    lab-1 v1.1

---
+++ lab1/Lab1/DigitsAnalyser.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab1
+{
+    class DigitsAnalyser
+    {
+        public int SplitNumber1(int number)
+        {
+            int[] result = new int[4];
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = number % 10;
+                number /= 10;
+            }
+            int summ = result[0] + result[1] + result[2] + result[3];
+            return summ;
+        }
+
+        public int SplitNumber2(int number)
+        {
+            int[] result = new int[4];
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = number % 10;
+                number /= 10;
+            }
+            int summ = result[0] * result[1] * result[2] * result[3];
+            return summ;
+        }
+
+    }
+}
---
+++ lab1/Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitsAnalyser digitsAnalyser = new DigitsAnalyser();
+            bool IsCorrect = false;
+            string s = "";
+            while (!IsCorrect)
+            {
+
+                Console.WriteLine("Введите четырехзначное число");
+                s = Console.ReadLine();
+                IsCorrect = true;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!char.IsDigit(s[i]))
+                    {
+                        IsCorrect = false;
+                    }
+                }
+                if (s.Length != 4)
+                {
+                    IsCorrect = false;
+                }
+            }
+
+            int k = Convert.ToInt32(s);
+            int summa = 0;
+            int multiplication = 1;
+
+
+            summa = digitsAnalyser.SplitNumber1(k);
+            multiplication = digitsAnalyser.SplitNumber2(k);
+
+            Console.WriteLine("Сумма цифр этого числа равна {0}, произведение цифр этого числа равно {1}", summa, multiplication);
+
+            Console.ReadLine();
+        }
+    }
+}

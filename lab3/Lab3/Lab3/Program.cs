cb839e0c8aa2ef2d8844adfbcd62c2fc01aed15a
Valeria2998 <velx8@mail.ru>
2018-04-10 10:58:49 +0400

    lab-3 v1.2

---
+++ lab3/Lab3/Lab3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random myRnd = new Random();
+            int[] array = new int[10];
+
+            Randomaser randomaser = new Randomaser();
+
+            Console.WriteLine("Исходный массив:");     
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRnd.Next(1, 10);
+                Console.Write(array[i] + " ");
+            }
+
+            array = randomaser.random(array);
+
+            Console.WriteLine("\nРезультат:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ lab3/Lab3/Lab3/Randomaser.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab3
+{
+    public class Randomaser
+    {
+        public int[] random(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0) array[i] = array[i] * 3;
+                else array[i] = 0;
+            }
+            return array;
+        }
+    }
+}

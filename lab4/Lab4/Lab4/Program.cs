5e7611f42eedb30dba0f7407a6d26e65b31eaac9
Valeria2998 <velx8@mail.ru>
2018-04-10 10:49:37 +0400

    lab-4 v1.0

---
+++ lab4/Lab4/Lab4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 10, 7, 8, 4, 2, 4, 1, 1, 3 };
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Swapper swapper = new Swapper();
+            int[] result = swapper.Swap(array);
+          
+            foreach (int i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ lab4/Lab4/Lab4/Swapper.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab4
+{
+    public class Swapper
+    {
+        public int[] Swap(int[] array)
+        {
+            int minIndex = 0, maxIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+                else if (array[i] < array[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+
+            if (minIndex > maxIndex)
+            {
+                Swap(ref minIndex, ref maxIndex);
+            }
+
+            for (int i = minIndex + 1, j = maxIndex - 1; i < j; i++, j--)
+            {
+                Swap(ref array[i], ref array[j]);
+            }
+
+            return array;
+        }
+
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
